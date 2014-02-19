0d5b23a98aa3908bc2200fa802df2f5f480d20fb
Dimitar Dimitrov <mitkofr@yahoo.fr>
2014-02-19 17:30:28 +0000

    Exclude more directories

--- backup.sh
+++ backup.sh
@@ -1,21 +1,30 @@
 #! /usr/bin/env bash
 
+# Usage: backup.sh /path/to/backup/folder
 # NB: /path/to/backup/folder must be excluded (ex: /mnt, /media...)
 
-# Archive, ACLs and extended attributes
-rsync -aAX /* /path/to/backup/folder \
---exclude={\
-/dev/*,\
-/home/*/.cache/chromium/*,\
-/home/*/.mozilla/firefox/*.default/Cache/*,\
-/home/*/.thumbnails/*,\
-/lost+found,\
-/media/*,\
-/mnt/*,\
-/proc/*,\
-/run/*,\
-/sys/*,\
-/tmp/*,\
-/var/lib/pacman/sync/*,\
-/var/log/journal/*\
+(($# == 1)) && [[ -d $1 ]] || {
+   echo 'Usage: backup.sh /path/to/backup/folder'
+   exit 1
 }
+
+# Archive, ACLs and extended attributes
+rsync -aAX --stats --progress /* "$1"                \
+--exclude=/dev/*                                     \
+--exclude=/home/*/.cache/*                           \
+--exclude=/home/*/.gvfs                              \
+--exclude=/home/*/.local/share/Trash                 \
+--exclude=/home/*/.mozilla/firefox/*.default/Cache/* \
+--exclude=/home/*/.thumbnails/*                      \
+--exclude=/lib/modules/*/volatile/.mounted           \
+--exclude=/lost+found                                \
+--exclude=/media/*                                   \
+--exclude=/mnt/*                                     \
+--exclude=/proc/*                                    \
+--exclude=/run/*                                     \
+--exclude=/sys/*                                     \
+--exclude=/tmp/*                                     \
+--exclude=/var/lib/pacman/sync/*                     \
+--exclude=/var/lock/*                                \
+--exclude=/var/log/journal/*                         \
+--exclude=/var/run/*
