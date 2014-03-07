3e736ec187f4600956137c0a6421d86e854230de
Dimitar Dimitrov <mitkofr@yahoo.fr>
2014-03-07 11:04:29 +0000

    Change default string

--- figlet_test_all.bash
+++ figlet_test_all.bash
@@ -2,17 +2,17 @@
 
 command -v figlet || exit 1
 
-                                     figlet             "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f slant:    \n" && figlet -f slant    "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f smslant:  \n" && figlet -f smslant  "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f small:    \n" && figlet -f small    "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f block:    \n" && figlet -f block    "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f lean:     \n" && figlet -f lean     "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f lean:     \n" && figlet -f lean     "${1:-ASUS Z87-PRO}" | tr ' _/' ' ()'
-printf "\nfiglet -f mini:     \n" && figlet -f mini     "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f digital:\n\n" && figlet -f digital  "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f bubble:   \n" && figlet -f bubble   "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f script:   \n" && figlet -f script   "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f smscript: \n" && figlet -f smscript "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f banner: \n\n" && figlet -f banner   "${1:-ASUS Z87-PRO}"
-printf "\nfiglet -f ivrit:    \n" && figlet -f ivrit    "${1:-ASUS Z87-PRO}"
+                                     figlet             "${1:-Intel i7 4770k}"
+printf "\nfiglet -f slant:    \n" && figlet -f slant    "${1:-Intel i7 4770k}"
+printf "\nfiglet -f smslant:  \n" && figlet -f smslant  "${1:-Intel i7 4770k}"
+printf "\nfiglet -f small:    \n" && figlet -f small    "${1:-Intel i7 4770k}"
+printf "\nfiglet -f block:    \n" && figlet -f block    "${1:-Intel i7 4770k}"
+printf "\nfiglet -f lean:     \n" && figlet -f lean     "${1:-Intel i7 4770k}"
+printf "\nfiglet -f lean:     \n" && figlet -f lean     "${1:-Intel i7 4770k}" | tr ' _/' ' ()'
+printf "\nfiglet -f mini:     \n" && figlet -f mini     "${1:-Intel i7 4770k}"
+printf "\nfiglet -f digital:\n\n" && figlet -f digital  "${1:-Intel i7 4770k}"
+printf "\nfiglet -f bubble:   \n" && figlet -f bubble   "${1:-Intel i7 4770k}"
+printf "\nfiglet -f script:   \n" && figlet -f script   "${1:-Intel i7 4770k}"
+printf "\nfiglet -f smscript: \n" && figlet -f smscript "${1:-Intel i7 4770k}"
+printf "\nfiglet -f banner: \n\n" && figlet -f banner   "${1:-Intel i7 4770k}"
+printf "\nfiglet -f ivrit:    \n" && figlet -f ivrit    "${1:-Intel i7 4770k}"
