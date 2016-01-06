ce2468fd28eb5ac22e2d42248a74c529970b29c4
Dimitar Dimitrov <mitkofr@yahoo.fr>
2016-01-06 10:54:05 +0000

    Improve + optimize cecho()

--- cecho.sh
+++ cecho.sh
@@ -1,10 +1,12 @@
 # Colorful messages
-#    * supports setting the following attributes:
-#      fg/bg color, bold (strong) and underlined
-#    * 256 colors support
+#    supports setting the following attributes:
+#      fg/bg color (-f/-b)
+#             bold (-s for strong)
+#       underlined (-u)
 # examples:
-#    cecho -fblue -bgreen -su 'message'
-#    cecho -f221 'message'
+#    cecho -fblue -bgreen -su message
+#    cecho -fylw:221 message # 256 colors support (ylw: is optional)
+# TODO: allow reading from a heredoc
 cecho() {
 
    local _bld="$(tput bold || tput md)"
@@ -13,10 +15,6 @@ cecho() {
 
    OPTIND=1
 
-   # f - foreground
-   # b - background
-   # s - strong
-   # u - underlined
    local opt
    while getopts ':f:b:su' opt
    do
@@ -30,47 +28,37 @@ cecho() {
       esac
    done
 
-   shift $((OPTIND-1))
+   shift "$((OPTIND-1))"
 
-   local fg
-   local bg
+   local color
 
-   # 0 - Black    4 - Blue
-   # 1 - Red      5 - Purple
-   # 2 - Green    6 - Cyan
-   # 3 - Yellow   7 - White
+   _get_color() {
+      case "$1" in
+          black) color=0 ;;
+            red) color=1 ;;
+          green) color=2 ;;
+         yellow) color=3 ;;
+           blue) color=4 ;;
+         purple) color=5 ;;
+           cyan) color=6 ;;
+          white) color=7 ;;
+         [0-9]*) color="$1" ;;
+              *) echo 'Unrecognized color' 1>&2 ;;
+      esac
+   }
+
+   local fg bg
+
    if [[ $_fg ]]
    then
-      # setaf -> foreground
-      # setab -> background
-      case "$_fg" in
-                                    black) fg="$(tput setaf 0      || tput AF 0)"      ;;
-                                      red) fg="$(tput setaf 1      || tput AF 1)"      ;;
-                                    green) fg="$(tput setaf 2      || tput AF 2)"      ;;
-                                   yellow) fg="$(tput setaf 3      || tput AF 3)"      ;;
-                                     blue) fg="$(tput setaf 4      || tput AF 4)"      ;;
-                                   purple) fg="$(tput setaf 5      || tput AF 5)"      ;;
-                                     cyan) fg="$(tput setaf 6      || tput AF 6)"      ;;
-                                    white) fg="$(tput setaf 7      || tput AF 7)"      ;;
-         [0-9]|[0-9][0-9]|[0-9][0-9][0-9]) fg="$(tput setaf "$_fg" || tput AF "$_fg")" ;;
-                                        *) echo 'Unrecognized fg color' 1>&2 ;;
-      esac
+      _get_color "${_fg#*:}"
+      fg="$(tput setaf "$color" || tput AF "$color")"
    fi
 
    if [[ $_bg ]]
    then
-      case "$_bg" in
-                                    black) bg="$(tput setab 0      || tput AB 0)"      ;;
-                                      red) bg="$(tput setab 1      || tput AB 1)"      ;;
-                                    green) bg="$(tput setab 2      || tput AB 2)"      ;;
-                                   yellow) bg="$(tput setab 3      || tput AB 3)"      ;;
-                                     blue) bg="$(tput setab 4      || tput AB 4)"      ;;
-                                   purple) bg="$(tput setab 5      || tput AB 5)"      ;;
-                                     cyan) bg="$(tput setab 6      || tput AB 6)"      ;;
-                                    white) bg="$(tput setab 7      || tput AB 7)"      ;;
-         [0-9]|[0-9][0-9]|[0-9][0-9][0-9]) bg="$(tput setab "$_bg" || tput AB "$_bg")" ;;
-                                        *) echo 'Unrecognized bg color' 1>&2 ;;
-      esac
+      _get_color "${_bg#*:}"
+      bg="$(tput setab "$color" || tput AB "$color")"
    fi
 
    [[ $_b ]] && echo -n "$_bld"
