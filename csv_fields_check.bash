0367cb390efce713452d0601a45ddceb86e59491
Dimitar Dimitrov <mitkofr@yahoo.fr>
2017-10-06 11:19:45 +0100

    Help

--- csv_fields_check.bash
+++ csv_fields_check.bash
@@ -8,6 +8,21 @@
 #
 # Usage: csv_fields_check.bash {document.csv}
 
+shopt -s extglob
+
+_help() {
+   local info='Usage: csv_fields_check.bash {document.csv}'
+   if (($1 == 0))
+   then echo "$info"
+   else echo "$info" >&2
+   fi
+   exit "$1"
+}
+
+[[ $1 == @(-h|--help) ]] && _help 0
+
+(($# != 1)) && _help 1
+
 # get the most often occurring number of fields
 nf="$(awk -F, '{print NF}' "$1" | sort | uniq -c | sort -rn | head -n1 | awk '{print $2}')"
 
