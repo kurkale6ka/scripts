b183ff1c9efb53818e3203410e9a12981ffaa934
Dimitar Dimitrov <mitkofr@yahoo.fr>
2018-03-18 10:52:18 +0000

    No git on remote hosts + other cleanup

--- mkconfig.sh
+++ mkconfig.sh
@@ -25,65 +25,17 @@ fi
 initial_setup() {
    mkdir -p "$REPOS_BASE"
 
-   if command -v git >/dev/null 2>&1
-   then
-      if cd "$REPOS_BASE"
-      then
-         echo "* ${_blu}Cloning repositories in ${_blue}${REPOS_BASE/$HOME/~}${_res}..."
-
-         [[ ! -d zsh     ]] && git clone git@github.com:kurkale6ka/zsh.git
-         [[ ! -d bash    ]] && git clone git@github.com:kurkale6ka/bash.git
-         [[ ! -d help    ]] && git clone git@github.com:kurkale6ka/help.git
-         [[ ! -d config  ]] && git clone git@github.com:kurkale6ka/config.git
-         [[ ! -d scripts ]] && git clone git@github.com:kurkale6ka/scripts.git
-         [[ ! -d vim     ]] && git clone git@github.com:kurkale6ka/vim.git
-
-         echo
-
-         if [[ -d config ]]
-         then
-            if ssh-add -l 1>/dev/null 2>&1
-            then
-               echo "* ${_blu}Configuring git${_res}..."
-               . "$REPOS_BASE"/config/git.bash
-            else
-               echo "${_red}Please upload your key to GitHub${_res}: ssh-keygen -b4096 -trsa" 1>&2
-               return 1
-            fi
-         fi
-      fi
-   elif [[ ! -d $REPOS_BASE/bash ]]
-   then
-      echo "${_red}git isn't installed. please upload manually your config files!${_res}" 1>&2
-      return 2
-   fi
-
    # XDG setup
-   [[ -z $XDG_CONFIG_HOME ]] && export XDG_CONFIG_HOME=~/.config
-   [[ -z   $XDG_DATA_HOME ]] && export   XDG_DATA_HOME=~/.local/share
+   . "$REPOS_BASE"/zsh/.zshenv
    mkdir -p "$XDG_CONFIG_HOME"
    mkdir -p "$XDG_DATA_HOME"
+   [[ ! -f $HOME/.zshenv ]] && cp "$REPOS_BASE"/zsh/.zshenv "$HOME"/.zshenv
 
    echo "* ${_blu}Linking dot files${_res}..."
    mklinks
 
    echo "* ${_blu}Creating fuzzy cd database${_res}..."
    . "$REPOS_BASE"/mkdb
-}
-
-updaterepos() {
-   for repo in "$REPOS_BASE"/*
-   do
-      if [[ -d $repo ]] && cd "$repo"
-      then
-         git fetch -q
-         if [[ $(git symbolic-ref --short HEAD) == master ]] && git status -sb | grep -q behind
-         then
-            echo -n "${_blu}${repo:t}${_res}: "
-            git pull
-         fi
-      fi
-   done
 }
 
 bash=(.bash_{profile,logout} .bashrc)
@@ -99,6 +51,15 @@ mklinks() {
    then
       # nvim
       ln -sfT "$REPOS_BASE"/vim "$XDG_CONFIG_HOME"/nvim
+
+      # zsh
+      if mkdir -p {"$XDG_CONFIG_HOME","$XDG_DATA_HOME"}/zsh
+      then
+         ln -sf "$REPOS_BASE"/zsh/.zshenv   ~
+         ln -sf "$REPOS_BASE"/zsh/autoload  "$XDG_CONFIG_HOME"/zsh
+         ln -sf "$REPOS_BASE"/zsh/.zprofile "$XDG_CONFIG_HOME"/zsh
+         ln -sf "$REPOS_BASE"/zsh/.zshrc    "$XDG_CONFIG_HOME"/zsh
+      fi
    else
       echo "mklinks: ${_red}XDG setup needed${_res}" 1>&2
    fi
@@ -126,9 +87,6 @@ mklinks() {
       do
          ln -sf "$REPOS_BASE"/scripts/"$c" ~/bin
       done
-
-      ln -sf "$REPOS_BASE"/vim/extra/vc ~/bin
-      ln -sf "$REPOS_BASE"/config/tmux/lay ~/bin
    fi
 }
 
@@ -141,6 +99,12 @@ rmlinks() {
    then
       # nvim
       'rm' "$XDG_CONFIG_HOME"/nvim
+
+      # zsh
+      'rm' ~/.zshenv
+      'rm' "$XDG_CONFIG_HOME"/zsh/autoload
+      'rm' "$XDG_CONFIG_HOME"/zsh/.zprofile
+      'rm' "$XDG_CONFIG_HOME"/zsh/.zshrc
    fi
 
    # Bash
@@ -164,25 +128,20 @@ rmlinks() {
    do
       'rm' ~/bin/"$c"
    done
-
-   'rm' ~/bin/vc
-   'rm' ~/bin/lay
 }
 
-opts[0]='Update repositories'
-opts[1]='Initial setup'
-opts[2]='Create fuzzy cd database'
-opts[3]='Make links'
-opts[4]='Remove links'
+opts[0]='Initial setup'
+opts[1]='Create fuzzy cd database'
+opts[2]='Make links'
+opts[3]='Remove links'
 
 select choice in "${opts[@]}"
 do
    case "$choice" in
-      "${opts[0]}") updaterepos;          break;;
-      "${opts[1]}") initial_setup;        break;;
-      "${opts[2]}") . "$REPOS_BASE"/mkdb; break;;
-      "${opts[3]}") mklinks;              break;;
-      "${opts[4]}") rmlinks;              break;;
+      "${opts[0]}") initial_setup;        break;;
+      "${opts[1]}") . "$REPOS_BASE"/mkdb; break;;
+      "${opts[2]}") mklinks;              break;;
+      "${opts[3]}") rmlinks;              break;;
                  *) echo '*** Wrong choice ***' >&2
    esac
 done
