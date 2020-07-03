23d3b4e5b2239ccaeb023b71b1e46c53e6731044
Dimitar Dimitrov <mitkofr@yahoo.fr>
2020-07-03 16:38:17 +0100

    Reorganize

--- mkssh.pl
+++ mkssh.pl
@@ -1,21 +1,37 @@
 #! /usr/bin/env perl
 
+# Install ssh keys:
+# - add users as needed
+# - ensure correct modes + permissions
+# TODO: allow changing of HOME
+
 use strict;
 use warnings;
 use feature 'say';
-use Term::ANSIColor ':constants';
+use Term::ANSIColor qw/color :constants/;
 use Getopt::Long 'GetOptions';
 use File::Path 'make_path';
-# use Term::ReadLine;
+use Term::ReadLine;
 
+my $YELLOW = color('yellow');
+my   $BOLD = color('bold');
+my  $RESET = color('reset');
+
+# Help
 sub help() {
-   print << 'MSG';
-mkssh
+   print <<MSG;
+mkssh            : ${YELLOW} read infile DATA${RESET}
 mkssh --stdin, -
+
+${BOLD}DESCRIPTION${RESET}
+Install ssh keys:
+- add users/folders as needed
+- ensure correct modes + permissions
 MSG
    exit;
 }
 
+# Arguments
 my $stdin;
 GetOptions(
    ''      => \$stdin,
@@ -23,7 +39,39 @@ GetOptions(
    'help'  => \&help
 ) or die RED.'Error in command line arguments'.RESET, "\n";
 
-# Install keys
+die RED.'No arguments allowed'.RESET, "\n" if @ARGV;
+
+# Declarations
+sub validate_key ($);
+sub install_keys (@);
+
+# Main
+if ($stdin)
+{
+   print 'Public key: ';
+   chomp ($_ = <STDIN>);
+   validate_key $_;
+   install_keys $_; # how to act by default on $_?
+} else {
+   my @keys;
+   while (<DATA>)
+   {
+      next if /^#/ or /^$/;
+      chomp;
+      validate_key $_;
+      push @keys, $_;
+   }
+   install_keys @keys;
+}
+
+# Functions
+sub validate_key ($)
+{
+   $_ = shift;
+   my @key = split;
+   @key == 3 or die RED.'Wrong ssh key format. "type key email" expected'.RESET, "\n";
+}
+
 sub install_keys (@)
 {
    my @keys = @_;
@@ -75,32 +123,9 @@ sub install_keys (@)
    }
 }
 
-sub validate_key ($)
-{
-   $_ = shift;
-   my @key = split;
-   @key == 3 or die RED.'Wrong ssh key format. "type key email" expected'.RESET, "\n";
-}
-
-if ($stdin)
-{
-   print 'Public key: ';
-   chomp ($_ = <STDIN>);
-   validate_key $_;
-   install_keys $_; # how to act by default on $_?
-} else {
-   my @keys;
-   while (<DATA>)
-   {
-      next if /^#/ or /^$/;
-      chomp;
-      validate_key $_;
-      push @keys, $_;
-   }
-   install_keys @keys;
-}
-
 __DATA__
+
+# keys: type key email
 
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDUHDwFoZ8CaKSwk/Wo1EQ104EHiJ+1HBuv7CByxwues46dGbhh0oXjW7jb0g5619kcUqCGLeEdYEtEDBugwj3N5bfVTKoHsbR9RHfu9DzhnUq+FnmWtRuk8oYZ/CUjojrxcNDjdr8NhVpKIIkp/5+isco9xSSPNUa6GQOwBbrnrREKaJf2YRTWcLu+9GULcma410OrqLy6jOKxc3IfrdZEL9HO9buSotCmQFw2uTu5CS+N6jG5M90LXNpYex/ZmXSmdwDym8qZ3FSlJcfP2NYXmDLvL6SfXBE43bdtXMMcQJM8/SOzmw91YYyu2bqACXEDvr8t6nYdcUsU8b6kXuGeZrgysbi446o9+EsDjF9YGQzjMi30zcMr8luvlqE1NlfnMaMsjI10ZxtD/NMJFMSSlO84JdT0JmaDtZw9x0J/D6xlUI6K9TGrFd6T9Ae3AQ3HagsyC7nPYU18wCZQTX9V8E3IJ60JkBXOZbcvD0dcZq/c/eIzYGEGX49qGjon8GJwNVNIYMwig1FQ+vUL51euItvrG/KsMoK0JadcrKcCQ3rG0Iy74Fwamdg2bLdDElFKxam7Pi7LpWhPe/ppMUeEFQDWTtmmJFoyTyeIE0sUn/OAzJ+xe/M0uYHINbaAelXNg3rOY8ECMOuwqoT2TauYCuIP+myOqsEdsBC1gxMA6w== dimitar.dimitrov@theengagehub.com
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDUHDwFo@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@2dGbhh0oXjW7jb0g5619kcUqCGLeEdYEtEDBugwj3N5bfVTKoHsbR9RHfu9DzhnUq+FnmWtRuk8oYZ/CUjojrxcNDjdr8NhVpKIIkp/5+isco9xSSPNUa6GQOwBbrnrREKaJf2YRTWcLu+9GULcma410OrqLy6jOKxc3IfrdZEL9HO9buSotCmQFw2uTu5CS+N6jG5M90LXNpYex/ZmXSmdwDym8qZ3FSlJcfP2NYXmDLvL6SfXBE43bdtXMMcQJM8/SOzmw91YYyu2bqACXEDvr8t6nYdcUsU8b6kXuGeZrgysbi446o9+EsDjF9YGQzjMi30zcMr8luvlqE1NlfnMaMsjI10ZxtD/NMJFMSSlO84JdT0JmaDtZw9x0J/D6xlUI6K9TGrFd6T9Ae3AQ3HagsyC7nPYU18wCZQTX9V8E3IJ60JkBXOZbcvD0dcZq/c/eIzYGEGX49qGjon8GJwNVNIYMwig1FQ+vUL51euItvrG/KsMoK0JadcrKcCQ3rG0Iy74Fwamdg2bLdDElFKxam7Pi7LpWhPe/ppMUeEFQDWTtmmJFoyTyeIE0sUn/OAzJ+xe/M0uYHINbaAelXNg3rOY8ECMOuwqoT2TauYCuIP+myOqsEdsBC1gxMA6w== dimitar.dimitrov@theengagehub.com
