5d5e0318d75249648f6bb019413554490ae9a854
Dimitar Dimitrov <mitkofr@yahoo.fr>
2020-08-06 17:07:53 +0100

    Play with clipboard

--- pc.pl
+++ pc.pl
@@ -7,27 +7,20 @@
 
 use strict;
 use warnings;
+use autodie;
 use feature 'say';
 use POSIX ":sys_wait_h";
 use File::Glob ':bsd_glob';
 use Term::ANSIColor qw/color :constants/;
 use Getopt::Long qw/GetOptions :config bundling/;
-use File::Basename 'basename';
-use File::Path 'make_path';
-use List::Util 'any';
 
 my $store = glob '~/.password-store';
-
-my $BLUE = color('ansi69');
-my $CYAN = color('ansi45');
-my $S = color('bold');
-my $R = color('reset');
 
 # Help
 sub help() {
    print << 'MSG';
 Fuzzy search and copy of passwords
-pc [-o|--stdout] [pattern] ... : copy password
+pc [-s store] [-o|--stdout] [pattern] ... : copy password
 MSG
    exit;
 }
@@ -35,9 +28,9 @@ MSG
 # Arguments
 my $stdout;
 GetOptions (
-   's|store'  => \$store,
-   'o|stdout' => \$stdout,
-   'h|help'   => \&help
+   's|store=s' => \$store,
+   'o|stdout'  => \$stdout,
+   'h|help'    => \&help
 ) or die RED.'Error in command line arguments'.RESET, "\n";
 
 chdir $store;
@@ -52,20 +45,26 @@ if (@ARGV)
    $pfile = `fd -e gpg -E'*~' -0 | sed -z 's/\\.gpg\$//' | fzf --read0 -0 -1 --cycle`;
 }
 
-system qw/pkill -f pc_cb_previous/;
+system qw/pkill pc_cb_prev/;
 
 my $clip_prev = `pbpaste`;
 
-my $pid = fork // die "failed to fork: $!";
+open my $paste, '|-', 'pbcopy';
+
+print $paste $pfile;
+# gpg -q -d $pstore/$pfile.gpg | head -n1 | tr -d '\n' | xclip
+
+my $pid = fork;
 
 # kid
 if ($pid == 0)
 {
-   $0 = 'pc_cb_previous';
-   sleep 45;
-   say "Hello World";
+   $0 = 'pc_cb_prev';
+
+   sleep 5;
+
+   print $paste $clip_prev;
    exit;
 }
 
-say $pfile;
 waitpid $pid, WNOHANG;
