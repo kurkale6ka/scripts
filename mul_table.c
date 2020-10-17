1a7255ef7b2db29afbf4e36595d543053872decd
Dimitar Dimitrov <mitkofr@yahoo.fr>
2020-10-17 13:50:21 +0100

    Use better formatting

--- mul_table.c
+++ mul_table.c
@@ -9,31 +9,58 @@ int main (int argc, char* argv[])
    short cols;
 
    if (argc > 1)
+   {
       cols = atoi(argv[1]);
+      if (cols < 1 || cols == 5 || (cols > 6 && cols < 12) || cols > 12)
+      {
+         printf("The possible columns are the divisors of 12: 1,2,3,4,6,12. "
+               "Try again!\n");
+         return 1;
+      }
+   }
    else
       cols = 4;
 
    for (short i = 1; i <= Twelve; i+=cols)
    {
-      // Headers
-      if (i > 1) printf("\n");
-
-      for (short k = 0; k <= cols-1; k++)
-         printf("%-13d   ", num++);
-
       // -------
-      printf("\n");
-      for (short i = 1; i <= 16*cols-3 ; i++)
-         printf("-");
+      if (i > 1)
+      {
+         for (short i = 1; i <= 16*cols-3 ; i++) printf("─");
+         printf("\n");
+      }
+
+      // Headers
+      for (short k = 0; k <= cols-1; k++)
+      {
+         switch(num++)
+         {
+            case    One: printf("     ONE        "); break;
+            case    Two: printf("     TWO        "); break;
+            case  Three: printf("    THREE       "); break;
+            case   Four: printf("    FOUR        "); break;
+            case   Five: printf("    FIVE        "); break;
+            case    Six: printf("     SIX        "); break;
+            case  Seven: printf("    SEVEN       "); break;
+            case  Eight: printf("    EIGHT       "); break;
+            case   Nine: printf("    NINE        "); break;
+            case    Ten: printf("     TEN        "); break;
+            case Eleven: printf("   ELEVEN       "); break;
+            case Twelve: printf("   TWELVE");        break;
+         }
+      }
       printf("\n");
 
       for (short j = 0; j <= Twelve; j++)
       {
          for (short k = 0; k <= cols-1; k++)
-            printf("%-2hd x %-2hd = %3hd   ", (short)(i+k), j, (short)((i+k)*j));
-
+         {
+            printf("%-2hd x %-2hd = %3hd", j, (short)(i+k), (short)((i+k)*j));
+            printf("%s", k < cols-1 ?  " │ " : "");
+         }
          printf("\n");
       }
    }
+
    return 0;
 }
