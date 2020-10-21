efa5d082bc7d2b5f262d4b812f5a3540c5201832
Dimitar Dimitrov <mitkofr@yahoo.fr>
2020-10-21 19:48:05 +0100

    mini

---
+++ mini.pl
@@ -0,0 +1,22 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+use feature 'say';
+
+chdir $ENV{REPOS_BASE} or die;
+
+my $clipboard = $^O eq 'darwin' ? 'pbcopy' : 'xclip';
+
+my @mini = qw(
+bash/.bashrc.mini
+config/dotfiles/.inputrc.mini
+config/ksh/.profile
+config/ksh/.kshrc
+vim/.vimrc.mini
+);
+
+$_ = `printf '%s\\n' @mini | fzf -0 -1 --cycle`;
+chomp;
+
+exec "$clipboard < $_" if $_;
