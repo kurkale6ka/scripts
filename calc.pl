04acf4598f0d4ffbbf43c482c90a5cce26358959
Dimitar Dimitrov <mitkofr@yahoo.fr>
2020-11-20 15:49:51 +0000

    calc

---
+++ calc.pl
@@ -0,0 +1,48 @@
+#! /usr/bin/env perl
+
+# SIMPLE calculator
+#
+# Usage: = math-expr
+#
+# Notes:
+#   symlink this script to =
+#   x can be used in lieu of *
+#   ^ can be used for powers (in addition to **)
+#   * can be omitted in parenthesised expressions: a(b+c)
+
+use strict;
+use warnings;
+use feature 'say';
+
+# read arguments
+if (@ARGV > 0)
+{
+   die "Usage: = math-expr\n" if $ARGV[0] =~ /-h|--help/i;
+   $_ = "@ARGV";
+} else {
+   chomp ($_ = <STDIN>);
+}
+
+# sanitize input
+unless (m@^[\s()'"\d%^x*/+-]*$@)
+{
+   s/\P{print}/?/g;
+   die substr ($_, 0, 17), ": bad symbols\n";
+}
+
+# allow pow with ^
+s/\^\^?/**/g;
+
+# allow x for multiplication
+s/x/*/g;
+
+# allow omitting * in parenthesised expressions: a(b+c)
+s/([\d)])\s*\(/$1*(/g if /[\d)]\s*\(/; # 2(5+7), )(
+s/\)\s*([\d])/)*$1/g if /\)\s*[\d]/;   # (5+7)2
+
+if ($_)
+{
+   say @ARGV == 0 ? '= ' : '', eval;
+} else {
+   warn "Usage: = math-expr\n";
+}
