55fff303c8527b28af060c0ff885fae70c9bbb18
Dimitar Dimitrov <mitkofr@yahoo.fr>
2020-12-04 22:51:53 +0000

    Allow fractions to be raised to the power with superscripts

--- calc.pl
+++ calc.pl
@@ -49,7 +49,7 @@ my $superscripts = '⁰¹²³⁴⁵⁶⁷⁸⁹';
 my $lparens = '（⟮﴾❨❪﹙';
 my $rparens = '）⟯﴿❩❫﹚';
 
-my $symbols = qr{^(['"\h()${lparens}${rparens}_.$fractions\d%^x×✕✖*÷∕/➕+−-]|(?<=[\d$rparens)])[$superscripts])*$}n;
+my $symbols = qr{^(['"\h()${lparens}${rparens}_.${fractions}\d%^x×✕✖*÷∕/➕+−-]|(?<=[\d${fractions}${rparens})])[$superscripts])*$}n;
 
 # Help
 sub help()
