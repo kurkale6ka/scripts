9815c9114d179f308b162f102e47a61bd38f8594
Dimitar Dimitrov <mitkofr@yahoo.fr>
2020-12-16 10:39:25 +0000

    Fail graciously on ^C inside fzf fix -d~/... with glob use $grep vs &grep so all options are parsed

--- ex.pl
+++ ex.pl
@@ -10,6 +10,7 @@ use feature 'say';
 use re '/aa';
 use Getopt::Long qw/GetOptions :config no_ignore_case bundling/;
 use Term::ANSIColor qw/color :constants/;
+use File::Glob ':bsd_glob';
 
 # Help
 sub help()
@@ -28,15 +29,20 @@ MSG
 
 # Arguments
 my $dir = '.';
-my ($hidden, $exact, $grep, $view);
+my ($hidden, $exact, $grep, $only, $view);
 GetOptions (
    'H|hidden'      => \$hidden,
    'd|directory=s' => \$dir,
    'e|exact'       => \$exact,
-   'g|grep'        => \&Grep,
+   'g|grep'        => \$grep,
+   'o|only'        => \$only,
    'v|view'        => \$view,
    'h|help'        => \&help
 ) or die RED.'Error in command line arguments'.RESET, "\n";
+
+$dir = glob $dir;
+
+$grep and Grep();
 
 sub Open
 {
@@ -55,9 +61,8 @@ sub Grep
 {
    chdir $dir or die RED.$!.RESET, "\n";
 
-   # FIXME: -g test -d~/help
    $_ = `rg -S --hidden -g'!.git' -g'!.svn' -g'!.hg' --ignore-file ~/.gitignore -l @ARGV | fzf -0 -1 --cycle --expect='alt-v' --preview "rg -Sn --color=always @ARGV $dir/{}"`;
    chomp;
 
-   Open $_;
+   Open $_ if $_;
 }
