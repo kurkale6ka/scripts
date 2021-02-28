983ed49193902facaa5acfb51040ef34d084543e
Dimitar Dimitrov <mitkofr@yahoo.fr>
2021-02-28 00:39:56 +0000

    Fix tests titles

--- calc.pl
+++ calc.pl
@@ -221,7 +221,7 @@ sub tests
       $res = math_eval();
       my $cl = ($res == $ans) ? GREEN : RED;
       my $rs = RESET;
-      printf "$cl%-25s$rs │ %s = %s ? $cl%s$rs\n", $title, $expr, $ans, $res;
+      printf "$cl%-32s$rs │ %s = %s ? $cl%s$rs\n", $title, $expr, $ans, $res;
    }
 }
 
@@ -229,33 +229,33 @@ __DATA__
 
 # Tests
 
-Multiplication            | 12_345_679 * 8                     | 98765432
-Multiplication ASCII x    | 12_345_679 x 9                     | 111111111
-Multiplication parens (   | 3(12-7)                            | 15
-Multiplication parens )   | (4-9)7                             | -35
-Multiplication Unicode    | 11 ✖ 8                             | 88
-Power ^                   | 2^3                                | 8
-Power superscript Unicode | 3³                                 | 27
-Division                  | 179 / 16                           | 11.1875
-Division Unicode          | 78 ÷ 3                             | 26
-Fractions Unicode 1)      | ¼ / ⅒                              | 2.5
-Fractions Unicode 2)      | ⅟4                                 | 0.25
-Fractions Unicode no *    | ⅒⅗                                 | 0.06
-Addition                  | 8 + 88                             | 96
-Addition Unicode          | 50 ➕ 101                          | 151
-Substraction              | 19 - 277                           | -258
-Substraction Unicode      | 231 − 17                           | 214
-Exponent notation e+      | 4e3                                | 4000
-Exponent notation e-      | 7e-2                               | 0.07
-Modulo                    | 17 % 3                             | 2
-Parens Unicode            | ⟮5+2⟯*（4-15）                     | -77
-Numbers Unicode           | 𝟭𝟥 + 𝟨𝟿                            | 82
-Squeeze                   | 3 --- 4                            | -1
-Squeeze power             | 3 ***************** 3              | 27
-Combined 1)               | -5e2 + 12                          | -488
-Combined 2)               | ❨4÷8❩⁷                             | 0.0078125
-Combined 3)               | ⅕e-12                              | 2e-13
-Combined 4)               | 3²/(2-19)(4+1.1) − 7(12-100) + 3^6 | 1342.3
-Combined 5)               | ⅒(3-5)                             | -0.2
-Memory _ set              | 17 - 39                            | -22
-Memory _ get              | _^2                                | -484
+Multiplication                   | 12_345_679 * 8                     | 98765432
+Multiplication ASCII x           | 12_345_679 x 9                     | 111111111
+Multiplication parens (          | 3(12-7)                            | 15
+Multiplication parens )          | (4-9)7                             | -35
+Multiplication Unicode           | 11 ✖ 8                             | 88
+Multiplication Unicode fractions | ⅒⅗                                 | 0.06
+Power ^                          | 2^3                                | 8
+Power superscript Unicode        | 3³                                 | 27
+Division                         | 179 / 16                           | 11.1875
+Division Unicode                 | 78 ÷ 3                             | 26
+Division fractions Unicode 1.    | ¼ / ⅒                              | 2.5
+Division fractions Unicode 2.    | ⅟4                                 | 0.25
+Addition                         | 8 + 88                             | 96
+Addition Unicode                 | 50 ➕ 101                          | 151
+Substraction                     | 19 - 277                           | -258
+Substraction Unicode             | 231 − 17                           | 214
+Exponent notation e+             | 4e3                                | 4000
+Exponent notation e-             | 7e-2                               | 0.07
+Modulo                           | 17 % 3                             | 2
+Parens Unicode                   | ⟮5+2⟯*（4-15）                     | -77
+Numbers Unicode                  | 𝟭𝟥 + 𝟨𝟿                            | 82
+Squeeze                          | 3 --- 4                            | -1
+Squeeze power                    | 3 ***************** 3              | 27
+Combined 1.                      | -5e2 + 12                          | -488
+Combined 2.                      | ❨4÷8❩⁷                             | 0.0078125
+Combined 3.                      | ⅕e-12                              | 2e-13
+Combined 4.                      | 3²/(2-19)(4+1.1) − 7(12-100) + 3^6 | 1342.3
+Combined 5.                      | ⅒(3-5)                             | -0.2
+Memory _ set                     | 17 - 39                            | -22
+Memory _ get                     | _^2                                | -484
