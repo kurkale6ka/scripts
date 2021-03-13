27a58d56d4faf023a3ca2f2b1424772bd8c16afe
Dimitar Dimitrov <mitkofr@yahoo.fr>
2021-03-13 15:02:09 +0000

    Fix remaining issues

--- pc.pl
+++ pc.pl
@@ -1,74 +1,84 @@
 #! /usr/bin/env perl
 
-# Fuzzy search and copy of passwords
+# Fuzzy password search, before copying to clipboard
 #
 # pass - the standard UNIX password manager
 # https://www.passwordstore.org/
 
 use v5.12;
 use warnings;
-use POSIX ':sys_wait_h';
 use Term::ANSIColor qw/color :constants/;
 use Getopt::Long qw/GetOptions :config bundling/;
 
+my $timeout = 15;
 my $store = "$ENV{HOME}/.password-store";
+my $copy  = $^O eq 'darwin' ? 'pbcopy'  : 'xclip';
+my $paste = $^O eq 'darwin' ? 'pbpaste' : 'xclip -o';
 
 # Help
-my $help = << '';
-Fuzzy search and copy of passwords
-pc [-s store] [-o|--stdout] [pattern] ... : copy password
+my $help = << 'MSG';
+pc [pattern] : fuzzy password search, before copying to clipboard
 
-# Arguments
+--stdout,  -o : show on terminal (and copy)
+--store,   -s : define password store
+--timeout, -t : clipboard timeout for the password
+MSG
+
+# Options
 my $stdout;
 GetOptions (
-   's|store=s' => \$store,
-   'o|stdout'  => \$stdout,
-   'h|help'    => sub { print $help; exit }
+   'o|stdout'    => \$stdout,
+   's|store=s'   => \$store,
+   't|timeout=i' => \$timeout,
+   'h|help'      => sub { print $help; exit }
 ) or die RED.'Error in command line arguments'.RESET, "\n";
 
 chdir $store or die RED.$!.RESET, "\n";
 
 # Get password
-my $pfile;
 if (@ARGV)
 {
-   $pfile = `fd -e gpg -E'*~' -0 | sed -z 's/\\.gpg\$//' | fzf -q"@ARGV" --read0 -0 -1 --cycle`;
+   $_ = `fd -e gpg -E'*~' -0 | sed -z 's/\\.gpg\$//' | fzf -q"@ARGV" --read0 -0 -1 --cycle`;
 } else {
-   $pfile = `fd -e gpg -E'*~' -0 | sed -z 's/\\.gpg\$//' | fzf --read0 -0 -1 --cycle`;
+   $_ = `fd -e gpg -E'*~' -0 | sed -z 's/\\.gpg\$//' | fzf --read0 -0 -1 --cycle`;
 }
 
-chomp $pfile;
+chomp (my $passfile = $_);
 
-# Speed up previous clipboard restoration
+# Wait until clipboard restoration
 if (my $pid = `pgrep -f pc_cb_prev`)
 {
-   kill 'TERM', $pid or die RED.'Failed to kill'.RESET, "\n";
+   warn RED.'Waiting for clipboard restoration...'.RESET, "\n";
+   sleep 1 while kill 0, $pid;
 }
 
 # Get previous clipboard item
-my $paste = $^O eq 'darwin' ? 'pbpaste' : 'xclip -o';
-# base64 encode to obfuscate password in 'ps'
-chomp (my $clip_prev = `$paste | openssl base64`);
+chomp (my $clip_prev = `$paste`);
 
 # Copy to clipboard
 if ($stdout)
 {
-   say YELLOW.$pfile.RESET;
-   system (qw/gpg -q -d/, "$store/$pfile.gpg") == 0 or die RED.$!.RESET, "\n";
+   say YELLOW.$passfile.RESET;
+   system (qw/gpg -q -d/, "$store/$passfile.gpg") == 0 or die RED.$!.RESET, "\n";
 } else {
-   say 'Copying ', color('yellow').$pfile.RESET, ' to the clipboard...';
+   say 'Copying ', color('yellow').$passfile.RESET, ' to the clipboard...';
 }
 
-system "gpg -q -d $store/$pfile.gpg | head -n1 | tr -d '\n' | pbcopy";
+system "gpg -q -d '$store/$passfile.gpg' | head -n1 | tr -d '\n' | $copy";
 $? == 0 or die RED.$!.RESET, "\n";
+
+# restore clipboard to previous entry
+sub restore
+{
+   exec "echo '$clip_prev' | tr -d '\n' | $copy";
+}
+
+restore() unless $clip_prev;
 
 # Wait in the background before restoring the clipboard
 exit if my $pid = fork // die "failed to fork: $!\n";
 
 # kid
-# keep password for 45sec, then reset clipboard to previous entry
 $0 = 'pc_cb_prev';
-sleep 45;
-
-# reset clipboard to previous entry
-exec "echo $clip_prev | openssl base64 -d | pbcopy";
+sleep $timeout;
+restore();
