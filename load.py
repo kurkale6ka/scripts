affdd7d863bc031ccdb43c56b013cbe82b676b1a
Dimitar Dimitrov <mitkofr@yahoo.fr>
2021-12-18 10:31:40 +0000

    Color

--- load.py
+++ load.py
@@ -1,8 +1,15 @@
 #! /usr/bin/env python3
 
+import re
+
 '''Load average'''
 
-import re
+class fg:
+   BOLD   = '\033[1m'
+   ITALIC = '\033[3m'
+   RESET  = '\033[0m'
+   GREEN  = '\033[32m'
+   RED    = '\033[31m'
 
 # load average
 with open('/proc/loadavg') as file:
@@ -22,5 +29,8 @@ with open('/proc/cpuinfo') as file:
 cores = 'cores' if count > 1 else 'core'
 
 # Output
-print('   1,    5,   15 : minutes')
-print(', '.join(load), ':', count, cores, 'load average', trend)
+load = [ (fg.GREEN if count > float(l) else fg.RED) + l + fg.RESET for l in load ]
+
+print('   1,    5,   15 :', fg.ITALIC + 'minutes' + fg.RESET)
+print(', '.join(load), ':', fg.BOLD + str(count) + fg.RESET, cores,
+      fg.ITALIC + 'load average', trend + fg.RESET)
