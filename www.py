5e5a35a823c80190b6917052ae7d8eba76b18026
Dimitar Dimitrov <mitkofr@yahoo.fr>
2021-12-22 15:15:38 +0000

    open

--- www.py
+++ www.py
@@ -2,14 +2,15 @@
 
 """Fuzzy search & open of websites loaded from a file"""
 
+import re
 import argparse
 from subprocess import run, Popen, PIPE
-from os import environ as env
+from os import execlp as exec, environ as env
 
 sites = env['XDG_DATA_HOME'] + '/sites'
 
 # Arguments
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser('www', description=f'fuzzy search & open of websites ({sites})')
 parser.add_argument("-s", "--sites", type=str, help="file with your bookmarked sites")
 parser.add_argument("pattern", nargs="?", type=str, help="site filter criteria")
 args = parser.parse_args()
@@ -23,4 +24,10 @@ with open(sites) as file:
    else:
       site = run(fzf, stdin=file, stdout=PIPE, text=True)
 
-print(site.stdout)
+pattern = re.compile('https?://\S+') # or
+# re.compile('www\.\S+') or
+# re.compile('\S+\.com\b')
+
+url = pattern.findall(site.stdout) [0]
+
+exec('open', 'open', url)
