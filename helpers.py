4e9affd278fe2eed42256dd88565224f7f819af8
Dimitar Dimitrov <mitkofr@yahoo.fr>
2022-01-06 11:12:07 +0000

    Display in columns

--- helpers.py
+++ helpers.py
@@ -1,5 +1,20 @@
-# from helpers import ls
-def ls(obj):
-   for member in dir(obj):
-      if not '__' in member:
-         print(member)
+# from helpers import *
+
+# aliases
+h = help
+p = print
+
+# dir() without __...__
+def ls(obj, columns=4, width=20):
+   '''List dir() entries in columns, omitting __...__'''
+
+   listing = [d for d in dir(obj) if not d.startswith('__')]
+
+   i = 0
+   for member in listing:
+      fields = listing[i:i+columns]
+      count = len(fields)
+      if fields:
+         fmt = ('{:' + str(width) + '}') * count
+         print(fmt.format(*fields))
+         i += count
