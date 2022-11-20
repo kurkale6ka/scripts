a6e4d868d9bdd18bc5ec09fdaa35bb12b0e0553e
Dimitar Dimitrov <mitkofr@yahoo.fr>
2022-11-20 09:27:52 +0000

    use gitpython module

--- mkconfig.py
+++ mkconfig.py
@@ -2,37 +2,27 @@
 
 '''Dot files setup'''
 
+from git import Repo
 from os import environ as env
 from os.path import basename
 from multiprocessing import Pool
-from subprocess import run
 from colorama import Fore as fg, Style as st
 
-class Repo:
-    base = env['HOME']+'/github/'
-
-    def __init__(self, path):
-        self.path = path
-
-    def name(self):
-        return basename(self.path)
-
-    def status(self):
-        cmd = ('git', '-C', self.path, '-c', 'color.status=always', 'status', '-sb')
-        st = run(cmd, capture_output=True, text=True)
-        return st.stdout.rstrip()
+base = env['HOME']+'/github/'
 
 repos = (
-Repo(Repo.base+'bash'),
-Repo(Repo.base+'config'),
-Repo(Repo.base+'help'),
-Repo(Repo.base+'scripts'),
-Repo(Repo.base+'vim'),
-Repo(Repo.base+'zsh')
+Repo(base+'bash'),
+Repo(base+'config'),
+Repo(base+'help'),
+Repo(base+'scripts'),
+Repo(base+'vim'),
+Repo(base+'zsh')
 )
 
 def get_status(repo):
-    return '{}{:>7}{}: {}'.format(fg.CYAN, repo.name(), fg.RESET, repo.status())
+    # return '{}{:>7}{}: {}'.format(fg.CYAN, repo.name(), fg.RESET, repo.status('-sb'))
+    if repo.is_dirty():
+        return basename(repo.git.working_dir) + repo.git.status('-sb')
 
 with Pool() as pool:
     for status in pool.imap(get_status, repos):
