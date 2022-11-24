62605c3be120f0cf625f252c26240f6af13c4b28
Dimitar Dimitrov <mitkofr@yahoo.fr>
2022-11-24 11:27:49 +0000

    cert stuff in python

---
+++ cert.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+'''Show Certificate/CSR info
+create CSR
+'''
+
+import OpenSSL.crypto as crypto
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("certificate", type=str, help="certificate file|URL")
+args = parser.parse_args()
+
+with open(args.certificate) as f:
+    cert = crypto.load_certificate(crypto.FILETYPE_PEM, f.read())
+
+print(cert.get_subject())
+print(cert.get_issuer())
