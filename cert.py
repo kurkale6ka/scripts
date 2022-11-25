229997a3a08bcc4617e2839c1e6eb72ccd6c8760
Dimitar Dimitrov <mitkofr@yahoo.fr>
2022-11-25 18:32:09 +0000

    decorators

--- cert.py
+++ cert.py
@@ -16,13 +16,38 @@ parser.add_argument("-d", "--dates", action="store_true", help="")
 parser.add_argument("-t", "--text", action="store_true", help="")
 args = parser.parse_args()
 
-with open(args.certificate, 'rb') as f:
-    cert = x509.load_pem_x509_certificate(f.read())
+def prefix(prefix):
+    def decorator(func):
+        def wrapper(*args):
+            return prefix + ': ' + func(*args)
+        return wrapper
+    return decorator
 
-print('{:>7}: {}'.format('subject', cert.subject.rfc4514_string()))
-print('{:>7}: {}'.format('issuer', cert.issuer.rfc4514_string()))
-print('{:>7}: {}'.format('from', cert.not_valid_before.strftime('%d %b %Y %R')))
-print('{:>7}: {}'.format('to', cert.not_valid_after.strftime('%d %b %Y %R')))
+class MyCertificate:
+
+    def __init__(self, certificate):
+        # self.x509 = None
+        with open(certificate, 'rb') as f:
+            self.cert = x509.load_pem_x509_certificate(f.read())
+
+    @property
+    def subject(self):
+        return self.cert.subject.rfc4514_string()
+
+    @property
+    @prefix('subject')
+    def label_subject(self):
+        return self.subject
+
+cert = MyCertificate(args.certificate)
+# cert.x509 = x509
+
+# print('{:>7}: {}'.format('subject', cert.subject.rfc4514_string()))
+# print('{:>7}: {}'.format('issuer', cert.issuer.rfc4514_string()))
+# print('{:>7}: {}'.format('from', cert.not_valid_before.strftime('%d %b %Y %R')))
+# print('{:>7}: {}'.format('to', cert.not_valid_after.strftime('%d %b %Y %R')))
 
 if args.subject:
     print(cert.subject)
+else:
+    print(cert.label_subject)
