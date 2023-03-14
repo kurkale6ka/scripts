8125d2ffbf82e418b857393d670ce5c69a775a72
Dimitar Dimitrov <dimitar.dimitrov@version1.com>
2023-03-14 11:05:56 +0000

    Format with black

--- load.py
+++ load.py
@@ -1,29 +1,35 @@
 #! /usr/bin/env python3
 
-'''Load average with clear time slices and trend'''
+"""Load average with clear time slices and trend"""
 
 from os import cpu_count
 
-esc = '\033['
-GREEN, RED, BOLD, ITALIC, RESET = [f'{esc}{code}m' for code in (32, 31, 1, 3, 0)]
+esc = "\033["
+GREEN, RED, BOLD, ITALIC, RESET = [f"{esc}{code}m" for code in (32, 31, 1, 3, 0)]
 
 # load average
-with open('/proc/loadavg') as file:
-   load = file.read().split() [:3]
+with open("/proc/loadavg") as file:
+    load = file.read().split()[:3]
 
 one, five, fifteen = map(float, load)
 
-trend = ''
+trend = ""
 if one > five or one > fifteen:
-   trend = '(increasing)'
+    trend = "(increasing)"
 
 # get cores count
-count = cpu_count()
-cores = 'cores' if count > 1 else 'core'
+count = cpu_count() or 0
+cores = "cores" if count > 1 else "core"
 
 # Output
-load = [ (GREEN if count > float(ld) else RED) + ld + RESET for ld in load ]
+load = [(GREEN if count > float(ld) else RED) + ld + RESET for ld in load]
 
-print('   1,    5,   15 :', ITALIC + 'minutes' + RESET)
-print(', '.join(load), ':', BOLD + str(count) + RESET, cores,
-      ITALIC + 'load average', trend + RESET)
+print("   1,    5,   15 :", ITALIC + "minutes" + RESET)
+print(
+    ", ".join(load),
+    ":",
+    BOLD + str(count) + RESET,
+    cores,
+    ITALIC + "load average",
+    trend + RESET,
+)
