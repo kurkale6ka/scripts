c82cdf841ff949e5d4192fe4115c9e9e44454ba1
Dimitar Dimitrov <dimitar.dimitrov@version1.com>
2023-03-23 10:44:30 +0000

    Make -3/-y mutually exclusive

--- cal.py
+++ cal.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-desc = '''Wrapper around the cal (gcal, ncal, cal) UNIX commands
+desc = """Wrapper around the cal (gcal, ncal, cal) UNIX commands
 aiming to standardise output and available options
 
     output:
@@ -8,73 +8,86 @@ aiming to standardise output and available options
     * always display days horizontally
 
     options: always provide -3/-y
-'''
+"""
 
 from os import execlp
 from shutil import which
 import argparse
 
-parser = argparse.ArgumentParser(prog='cal', description=desc, formatter_class=argparse.RawDescriptionHelpFormatter, add_help=False)
+parser = argparse.ArgumentParser(
+    prog="cal",
+    description=desc,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    add_help=False,
+)
 
-parser.add_argument("-h", "--help",  action="store_true", help="show own help + \"cal\"'s one")
-parser.add_argument("-3", "--three", action="store_true", help="three months display")
-parser.add_argument("-y", "--year",  action="store_true", help="whole year display")
+grp_period = parser.add_mutually_exclusive_group()
+grp_period.add_argument(
+    "-3", "--three", action="store_true", help="three months display"
+)
+grp_period.add_argument("-y", "--year", action="store_true", help="whole year display")
+parser.add_argument(
+    "-h",
+    "--help",
+    action="store_true",
+    help='show this help, followed by the resident "cal"\'s one',
+)
 
 # Store own args as argparse.Namespace and the rest as list
 own_args, args = parser.parse_known_args()
+
 
 # check if command is in PATH
 def cmd(exe):
     return which(exe) is not None
 
-if cmd('gcal'):
 
+if cmd("gcal"):
     # -s, --starting-day 1 (Monday)
-    cal = ['gcal', '-s1']
+    cal = ["gcal", "-s1"]
 
     if own_args.three:
         # previous, actual and next month: month mode commands
-        cal.append('.')
+        cal.append(".")
     elif own_args.year:
         # -b, --blocks: displays 4 blocks with 3 months at a time (4x3 = 12)
-        cal.append('-b4')
+        cal.append("-b4")
 
-elif cmd('ncal'):
-
+elif cmd("ncal"):
     # -M Monday, -b oldstyle format: horizontal
-    cal = ['ncal', '-Mb']
+    cal = ["ncal", "-Mb"]
 
     # needed as we shadow ncal's ones
     if own_args.three:
-        cal.append('-3')
+        cal.append("-3")
     elif own_args.year:
-        cal.append('-y')
+        cal.append("-y")
 
-elif cmd('cal'):
-
+elif cmd("cal"):
     # UNIX/FreeBSD version of cal:
     # cal -m could fail as support for -m3y was unknown last time I checked
 
     # -m, --monday
-    cal = ['cal', '-m']
+    cal = ["cal", "-m"]
 
     if own_args.three:
-        cal.append('-3')
+        cal.append("-3")
     elif own_args.year:
-        cal.append('-y')
+        cal.append("-y")
 
 else:
-    exit('Please install gcal, ncal or cal!')
+    exit("Please install gcal, ncal or cal!")
 
 cal.extend(args)
 
-if __name__  == "__main__":
-
-    # show own help + "cal"'s one
+if __name__ == "__main__":
     if own_args.help:
+        # this help
         parser.print_help()
-        print('\n', '-' * 54, '\n', sep='')
-        execlp(cal[0], cal[0], '--help')
+        print("\n", "-" * 54, "\n", sep="")
+
+        # resident "cal"'s help
+        execlp(cal[0], cal[0], "--help")
 
     # launch "cal"
     execlp(cal[0], *cal)
