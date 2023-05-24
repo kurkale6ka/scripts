ebb5f759263c5da2fb133bf9039eaeb5a6fd95ef
Dimitar Dimitrov <dimitar.dimitrov@version1.com>
2023-05-24 10:13:45 +0100

    hmmm

--- mini.py
+++ mini.py
@@ -1,56 +1,76 @@
 #! /usr/bin/env python3
 
-"""Copy stuff
+"""Copy mini configs for pasting on remote systems
+
+- inputrc
+- Bash/Korn rcs
+- vimrc
 """
 
-import argparse
+from argparse import ArgumentParser, RawTextHelpFormatter
 from dataclasses import dataclass
 from os import PathLike
 from pathlib import Path
 from subprocess import run, PIPE
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("tool", type=str, default="bash", help="get config(s) for tool")
+parser = ArgumentParser(description=__doc__, formatter_class=RawTextHelpFormatter)
+parser.add_argument("-a", "--all", action="store_true", help="choose from all configs")
+parser.add_argument(
+    "tool", type=str, default="bash", nargs="?", help="get config(s) for tool"
+)
 args = parser.parse_args()
 
 
 @dataclass
 class MiniConfig:
     name: str
-    location: PathLike = Path(".")
+    info: str = ""
+    path: PathLike = Path(".")
     comments: str = "#"
 
 
-configs = {
-    "readline": MiniConfig("inputrc", location=Path("config/dotfiles/.inputrc.mini")),
-    "bash": MiniConfig("bashrc", location=Path("bash/.bashrc.mini")),
-    "editor": MiniConfig("vimrc", location=Path("vim/.vimrc.mini")),
-    "ksh_profile": MiniConfig("profile", location=Path("config/ksh/.profile.mini")),
-    "ksh": MiniConfig("kshrc", location=Path("config/ksh/.kshrc.mini")),
+inputrc = MiniConfig(
+    "inputrc", info="readline", path=Path("config/dotfiles/.inputrc.mini")
+)
+bashrc = MiniConfig("bashrc", path=Path("bash/.bashrc.mini"))
+vimrc = MiniConfig("vimrc", path=Path("vim/.vimrc.mini"))
+profile = MiniConfig(
+    "profile", info="ksh profile", path=Path("config/ksh/.profile.mini")
+)
+kshrc = MiniConfig("kshrc", path=Path("config/ksh/.kshrc.mini"))
+
+mini_configs = {
+    inputrc.name: inputrc,
+    bashrc.name: bashrc,
+    vimrc.name: vimrc,
+    profile.name: profile,
+    kshrc.name: kshrc,
 }
 
-bash = [
-    configs["readline"],
-    configs["bash"],
-    configs["editor"],
-]
-
-ksh = [
-    configs["ksh_profile"],
-    configs["ksh"],
-    configs["editor"],
-]
-
 if __name__ == "__main__":
-    if args.tool in ("bash", "ksh"):
-        # config = args.tool
-        pass
+    filter = ("fzf", "-q", args.tool, "-0", "-1", "--cycle")
+    if args.all:
+        filter = ("fzf", "--cycle")
+
+    if not args.all and args.tool in ("bash", "ksh"):  # TODO: use regex?
+        if args.tool == "bash":
+            print(mini_configs[inputrc.name].path)
+            print(mini_configs[bashrc.name].path)
+            print(mini_configs[vimrc.name].path)
+        else:
+            print(mini_configs[profile.name].path)
+            print(mini_configs[kshrc.name].path)
+            print(mini_configs[vimrc.name].path)
     else:
-        filter = ("fzf", "-q", args.tool, "-0", "-1", "--cycle")
-        config = run(filter, input="\n".join(configs.keys()), stdout=PIPE, text=True)
+        configs = "\n".join(
+            f"{cfg.name}: {cfg.info}" if cfg.info else cfg.name
+            for cfg in mini_configs.values()
+        )
+        config = run(filter, input=configs, stdout=PIPE, text=True)
         if config.returncode == 130:
             exit("canceled")
         else:
             config = config.stdout.rstrip()
-        print(config)
+            config = config.split(":")[0]
+        print(mini_configs[config].path)
