26780a4df28eae9f62fdf9039dc732f905b851f2
Dimitar Dimitrov <dimitar.dimitrov@version1.com>
2023-05-25 17:22:51 +0100

    Super Simplify

--- mini.py
+++ mini.py
@@ -34,11 +34,12 @@ class MiniConfig:
     def __post_init__(self):
         self.path = Path(f"{env['REPOS_BASE']}/github/{self.path}")
 
-    def get(self, file):
-        out = f"cat >> ~/.{self.name} << '{self.name.upper()}'\n"
-        out += f"{self.comments} {'-' * 78}\n"
-        out += file.read()  # TODO: open the file here
-        out += self.name.upper()
+    def get(self):
+        with open(self.path) as config:
+            out = f"cat >> ~/.{self.name} << '{self.name.upper()}'\n"
+            out += f"{self.comments} {'-' * 78}\n"
+            out += config.read()
+            out += self.name.upper()
         return out
 
 
@@ -46,7 +47,7 @@ inputrc = MiniConfig(
     "inputrc", info="readline", path=Path("config/dotfiles/.inputrc.mini")
 )
 bashrc = MiniConfig("bashrc", path=Path("bash/.bashrc.mini"))
-vimrc = MiniConfig("vimrc", path=Path("vim/.vimrc.mini"))
+vimrc = MiniConfig("vimrc", path=Path("vim/.vimrc.mini"), comments='"')
 profile = MiniConfig(
     "profile", info="ksh profile", path=Path("config/ksh/.profile.mini")
 )
@@ -67,15 +68,17 @@ if __name__ == "__main__":
 
     if not args.all and args.tool in ("bash", "ksh"):  # TODO: use regex?
         if args.tool == "bash":
-            # cat config
-            with open(inputrc.path) as f_inputrc, open(bashrc.path) as f_bashrc, open(
-                vimrc.path
-            ) as f_vimrc:
-                print(inputrc.get(f_inputrc))
+            print(inputrc.get())
+            print()
+            print(bashrc.get())
+            print()
+            print(vimrc.get())
         else:
-            print(profile.path)
-            print(kshrc.path)
-            print(vimrc.path)
+            print(profile.get())
+            print()
+            print(kshrc.get())
+            print()
+            print(vimrc.get())
     else:
         configs = "\n".join(
             f"{cfg.name}: {cfg.info}" if cfg.info else cfg.name
@@ -89,6 +92,4 @@ if __name__ == "__main__":
             config = config.stdout.rstrip()
             config = config.split(":")[0]
 
-        # cat config
-        with open(mini_configs[config].path) as conf:
-            print(conf.read().rstrip())
+        print(mini_configs[config].get())
