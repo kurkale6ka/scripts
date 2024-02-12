e28868dfd8252c58bf2d4c2c91179b56d02373a5
Dimitar Dimitrov <dimitar.dimitrov@version1.com>
2024-02-12 16:11:47 +0000

    Fix a few issues

--- cd.py
+++ cd.py
@@ -7,7 +7,10 @@ Fuzzy cd
 import argparse
 import os
 from os import environ as env
+import re
 from pathlib import Path
+from collections import Counter
+import operator
 
 
 class CDPaths:
@@ -15,35 +18,44 @@ class CDPaths:
         with open(histfile) as file:
             paths = []
             for line in file:
-                cmd = line.strip()
-                if cmd.startswith("cd "):
-                    dir = cmd.split(None, 1)[1]
-                    if all(not d in Path(dir).parts for d in [".git", ".venv"]):
-                        paths.append(dir.replace("~", env["HOME"]))
+                if line.lstrip().startswith("cd "):
+                    # cd /path && echo hi
+                    dir = line.split("&&")[0].split(None, 1)[1]
+
+                    # cd -- -hello
+                    if "-- " in dir:
+                        dir = dir.split("--")[1]
+
+                    # checking the regex should be faster than checking for Path existence
+                    if not re.fullmatch("-\\d*", dir):
+                        if all(not d in Path(dir).parts for d in [".git", ".venv"]):
+                            paths.append(Path(dir.strip().replace("~", env["HOME"])))
         self._paths = paths
 
-    def _mkdir(self):
+    def get(self):
         paths = []
         for p in self._paths:
-            if Path(p).exists():
-                paths.append(Path(p).resolve())
-            elif Path(env["HOME"] + "/" + p).exists():
-                paths.append(Path(env["HOME"] + "/" + p).exists())
+            if p.exists():
+                paths.append(p)
             else:
-                new_path = set(
-                    Path(a + "/" + p)
-                    for a in self._absolute_paths()
-                    if Path(a + "/" + p).exists()
-                )
-                if new_path:
-                    paths.append(new_path.pop())
-        return paths
+                h_path = Path(env["HOME"]).joinpath(p)
+                if h_path.exists():
+                    paths.append(h_path)
+                # too slow
+                # else:
+                #     for a in self._absolute_paths():
+                #         a_path = Path(a).joinpath(p)
+                #         if a_path.exists():
+                #             paths.append(a_path)
+                #             break
+        return sorted(
+            Counter(p.resolve().as_posix() for p in paths).items(),
+            key=operator.itemgetter(1),
+            reverse=True,
+        )
 
-    def _absolute_paths(self):
-        return [p for p in self._paths if Path(p).exists() and Path(p).is_absolute()]
-
-    def get(self):
-        return self._mkdir()
+    # def _absolute_paths(self):
+    #     return [p for p in self._paths if p.is_absolute()]
 
 
 if __name__ == "__main__":
@@ -60,4 +72,6 @@ if __name__ == "__main__":
 
     cd_paths = CDPaths(args.histfile)
 
-    print(cd_paths.get())
+    # print(cd_paths.get())
+    for p in cd_paths.get():
+        print(p[0])
