0152ebe4717a2ecb0534232d4b48f7636b4248a8
Dimitar Dimitrov <kurkale6ka@gmail.com>
2025-03-31 19:53:06 +0100

    Make 'dir' positional

--- textdir/src/td/__init__.py
+++ textdir/src/td/__init__.py
@@ -1,7 +1,11 @@
 #! /usr/bin/env python3
 
-"""Export a directory to files in a single text document"""
+"""Export a directory to a single text document"""
 
+# TODO:
+# - usage with redir
+# - classes google code style
+# - pytests
 import argparse
 from os import environ as env
 from pathlib import Path
@@ -12,6 +16,52 @@ from typing import NamedTuple
 class File(NamedTuple):
     path: Path
     contents: str
+
+
+class Folder:
+    def __init__(self, path: Path):
+        self._path = path
+
+    def to_files(self, sep: str) -> str:
+        fd = [
+            'fd',
+            f'--ignore-file={env["XDG_CONFIG_HOME"]}/git/ignore',
+            '-tf',
+            '-a',
+            '.',
+            str(self._path),
+        ]
+
+        # run fd or fdfind
+        try:
+            proc = run(fd, capture_output=True, text=True)
+        except FileNotFoundError:
+            fd[0] = 'fdfind'
+            proc = run(fd, capture_output=True, text=True)
+
+        if proc.returncode == 0:
+            files: list[File] = []
+
+            for path in proc.stdout.splitlines():
+                with open(path) as file:
+                    try:
+                        contents = file.read().rstrip()
+                    except UnicodeDecodeError:
+                        # TODO: stderr + colors
+                        print('Problem:', path)
+                    else:
+                        # skip empty files
+                        if contents:
+                            files.append(
+                                File(
+                                    Path(path).relative_to(self._path.resolve().parent),
+                                    contents,
+                                )
+                            )
+        else:
+            exit(proc.stderr.rstrip())
+
+        return sep.join(f'{file.path}\n{file.contents}' for file in files)
 
 
 class Files:
@@ -30,70 +80,33 @@ class Files:
                 file.write(contents)
 
 
-class Folder:
-    def __init__(self, path: Path):
-        self._path = path
-
-    def to_files(self, sep: str) -> str:
-        fd = [
-            'fd',
-            '-tf',
-            '-a',
-            '-p',
-            f'--ignore-file={env["XDG_CONFIG_HOME"]}/git/ignore',
-            str(self._path),
-        ]
-
-        # run fd or fdfind
-        try:
-            proc = run(fd, capture_output=True, text=True)
-        except FileNotFoundError:
-            fd[0] = 'fdfind'
-            proc = run(fd, capture_output=True, text=True)
-
-        if proc.returncode == 0:
-            files: list[File] = []
-            for path in proc.stdout.splitlines():
-                with open(path) as file:
-                    contents = file.read().rstrip()
-
-                # skip empty files
-                if contents:
-                    files.append(
-                        File(
-                            Path(path).relative_to(self._path.resolve().parent),
-                            contents,
-                        )
-                    )
-        else:
-            exit(proc.stderr.rstrip())
-
-        return sep.join(f'{file.path}\n{file.contents}' for file in files)
-
-
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
-        '-d', '--dir', type=Path, default=Path('.'), help='source directory'
-    )
-    parser.add_argument('-s', '--source', type=Path, help='recreate folder from files')
-    parser.add_argument(
-        '-t',
-        '--to-files',
+        '-e',
+        '--export',
         action='store_true',
-        help='export directory to files in a single text document',
+        help='export directory as a single stream',
+    )
+    parser.add_argument(
+        '-i',
+        '--import-source',
+        metavar='SOURCE',
+        type=Path,
+        help='import directory from source file',
+    )
+    parser.add_argument(
+        'dir', nargs='?', type=Path, default=Path('.'), help='source directory'
     )
     args = parser.parse_args()
 
     # Main
     sep = '\n----------------------- File -----------------------\n'
 
-    if args.to_files:
-        dir = Folder(args.dir)
-        print(dir.to_files(sep))
+    if args.export:
+        print(Folder(args.dir).to_files(sep))
     else:
-        files = Files(args.source)
-        files.to_folder(sep)
+        Files(args.import_source).to_folder(sep)
 
 
 if __name__ == '__main__':
